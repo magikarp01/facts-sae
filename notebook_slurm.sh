01466a797c3e3eb183cc65b5a72e4d7168498a25
magikarp01 <philliphguo@gmail.com>
2024-01-24 08:57:22 +0000

    Evaluated SAE L0 and loss recovered

--- notebook_slurm.sh
+++ notebook_slurm.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --output=jupyter_logs/log-%J.txt
-#SBATCH --nodes=2
-# #SBATCH --tasks-per-node=1
-# #SBATCH --cpus-per-task=4
-# #SBATCH --gres=gpu:2
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
 #SBATCH --time=4:00:00
 
 
-# source /data/phillip_guo/miniconda3/etc/profile.d/conda.sh
-conda activate unlrn
-
+source /data/phillip_guo/miniconda3/etc/profile.d/conda.sh
+conda activate unlrn-2
 jupyter-notebook --no-browser --ip=0.0.0.0 --NotebookApp.kernel_name=unlrn --NotebookApp.allow_origin_pat=https://.*vscode-cdn\.net --NotebookApp.allow_origin='*'
